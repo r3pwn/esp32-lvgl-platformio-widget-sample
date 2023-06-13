a2bfae519790a96f9d7a37ce20782abc31a74494
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-06-13 00:59:49 -0400

    [chore] code cleanup

--- src/LvglWidgets.cpp
+++ src/LvglWidgets.cpp
@@ -18,7 +18,6 @@ class LGFX : public lgfx::LGFX_Device
 {
 
   lgfx::Panel_GC9A01 _panel_instance;
- // lgfx::Light_PWM     _light_instance;
   lgfx::Bus_SPI _bus_instance;
 
 public:
@@ -27,62 +26,51 @@ public:
     {
       auto cfg = _bus_instance.config();
 
-      // SPIバスの設定
-      cfg.spi_host = SPI2_HOST; // 使用するSPIを選択  ESP32-S2,C3 : SPI2_HOST or SPI3_HOST / ESP32 : VSPI_HOST or HSPI_HOST
-      // ※ ESP-IDFバージョンアップに伴い、VSPI_HOST , HSPI_HOSTの記述は非推奨になるため、エラーが出る場合は代わりにSPI2_HOST , SPI3_HOSTを使用してください。
-      cfg.spi_mode = 0;                  // SPI通信モードを設定 (0 ~ 3)
-      cfg.freq_write = 80000000;         // 传输时的SPI时钟（最高80MHz，四舍五入为80MHz除以整数得到的值）
-      cfg.freq_read = 20000000;          // 接收时的SPI时钟
-      cfg.spi_3wire = true;              // 受信をMOSIピンで行う場合はtrueを設定
-      cfg.use_lock = true;               // 使用事务锁时设置为 true
-      cfg.dma_channel = SPI_DMA_CH_AUTO; // 使用するDMAチャンネルを設定 (0=DMA不使用 / 1=1ch / 2=ch / SPI_DMA_CH_AUTO=自動設定)
-      // ※ ESP-IDFバージョンアップに伴い、DMAチャンネルはSPI_DMA_CH_AUTO(自動設定)が推奨になりました。1ch,2chの指定は非推奨になります。
-      cfg.pin_sclk = 6;  // SPIのSCLKピン番号を設定
-      cfg.pin_mosi = 7;  // SPIのCLKピン番号を設定
-      cfg.pin_miso = -1; // SPIのMISOピン番号を設定 (-1 = disable)
-      cfg.pin_dc = 2;    // SPIのD/Cピン番号を設定  (-1 = disable)
+      cfg.spi_host = SPI2_HOST;
+      
+      cfg.spi_mode = 0;
+      cfg.freq_write = 80000000;
+      cfg.freq_read = 20000000;
+      cfg.spi_3wire = true;
+      cfg.use_lock = true;
+      cfg.dma_channel = SPI_DMA_CH_AUTO;
+      
+      cfg.pin_sclk = 6;
+      cfg.pin_mosi = 7;
+      cfg.pin_miso = -1;
+      cfg.pin_dc = 2;
 
-      _bus_instance.config(cfg);              // 設定値をバスに反映します。
-      _panel_instance.setBus(&_bus_instance); // バスをパネルにセットします。
+      _bus_instance.config(cfg);
+      _panel_instance.setBus(&_bus_instance);
     }
 
-    {                                      // 表示パネル制御の設定を行います。
-      auto cfg = _panel_instance.config(); // 表示パネル設定用の構造体を取得します。
+    {
+      auto cfg = _panel_instance.config();
 
-      cfg.pin_cs = 10;   // CSが接続されているピン番号   (-1 = disable)
-      cfg.pin_rst = -1;  // RSTが接続されているピン番号  (-1 = disable)
-      cfg.pin_busy = -1; // BUSYが接続されているピン番号 (-1 = disable)
+      cfg.pin_cs = 10;
+      // disable the RST and BUSY pins
+      cfg.pin_rst = -1;
+      cfg.pin_busy = -1;
 
-      // ※ 以下の設定値はパネル毎に一般的な初期値が設定さ BUSYが接続されているピン番号 (-1 = disable)れていますので、不明な項目はコメントアウトして試してみてください。
-
-      cfg.memory_width = 240;   // ドライバICがサポートしている最大の幅
-      cfg.memory_height = 240;  // ドライバICがサポートしている最大の高さ
-      cfg.panel_width = 240;    // 実際に表示可能な幅
-      cfg.panel_height = 240;   // 実際に表示可能な高さ
-      cfg.offset_x = 0;         // パネルのX方向オフセット量
-      cfg.offset_y = 0;         // パネルのY方向オフセット量
-      cfg.offset_rotation = 0;  // 值在旋转方向的偏移0~7（4~7是倒置的） 
-      cfg.dummy_read_pixel = 8; // 在读取像素之前读取的虚拟位数
-      cfg.dummy_read_bits = 1;  // 读取像素以外的数据之前的虚拟读取位数
-      cfg.readable = false;     // 如果可以读取数据，则设置为 true
-      cfg.invert = true;        // 如果面板的明暗反转，则设置为 true
-      cfg.rgb_order = false;    // 如果面板的红色和蓝色被交换，则设置为 true
-      cfg.dlen_16bit = false;   // 对于以 16 位单位发送数据长度的面板，设置为 true
-      cfg.bus_shared = false;   // 如果总线与 SD 卡共享，则设置为 true（使用 drawJpgFile 等执行总线控制）
+      cfg.memory_width = 240;
+      cfg.memory_height = 240;
+      cfg.panel_width = 240;
+      cfg.panel_height = 240;
+      cfg.offset_x = 0;
+      cfg.offset_y = 0;
+      cfg.offset_rotation = 0;
+      cfg.dummy_read_pixel = 8;
+      cfg.dummy_read_bits = 1;
+      cfg.readable = false;
+      cfg.invert = true;
+      cfg.rgb_order = false;
+      cfg.dlen_16bit = false;
+      cfg.bus_shared = false;
 
       _panel_instance.config(cfg);
     }
 
-    setPanel(&_panel_instance); // 使用するパネルをセットします。
-//    { // バックライト制御の設定を行います。(必要なければ削除）
-//    auto cfg = _light_instance.config();// バックライト設定用の構造体を取得します。
-//    cfg.pin_bl = 8;             // バックライトが接続されているピン番号 BL
-//    cfg.invert = false;          // バックライトの輝度を反転させる場合 true
-//    cfg.freq   = 44100;          // バックライトのPWM周波数
-//    cfg.pwm_channel = 7;         // 使用するPWMのチャンネル番号
-//    _light_instance.config(cfg);
-//    _panel_instance.setLight(&_light_instance);//バックライトをパネルにセットします。
-//    }
+    setPanel(&_panel_instance);
   }
 };
 
@@ -158,8 +146,6 @@ void setup()
   Serial.println("I am LVGL_Arduino");
 
   ticker.attach(1, tcr1s);
-//  tft.setBrightness(200);
-//  tft.setSwapBytes(true);
   tft.init();
   tft.initDMA();
   tft.startWrite();
@@ -190,31 +176,14 @@ void setup()
   indev_drv.read_cb = my_touchpad_read;
   lv_indev_drv_register(&indev_drv);
 
-#if 0
-  /*Create a screen*/
-  lv_obj_t * scr = lv_obj_create(NULL, NULL);
-  lv_scr_load(scr);
-  /*Load the screen*/
-  /* Create simple label */
-   
-  lv_obj_t *label = lv_label_create( lv_scr_act() );
-  lv_label_set_text( label, "Hello Arduino! (V8.0.X)" );
-  lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
-#else
-  /* Try an example from the lv_examples Arduino library
-      make sure to include it as written above.
-   lv_example_btn_1();
-   */
-
   // uncomment one of these demos
-      lv_demo_widgets(); // OK
-                     // lv_demo_benchmark(); // OK
-                     //  lv_demo_keypad_encoder();
-                     //  works, but I haven't an encoder
-                     //  lv_demo_music();              // NOK
-                     //  lv_demo_printer();
-                     //  lv_demo_stress();             // seems to be OK
-#endif
+  lv_demo_widgets(); // OK
+  //  lv_demo_benchmark(); // OK
+  //  lv_demo_keypad_encoder();
+  //  works, but I haven't an encoder
+  //  lv_demo_music();              // NOK
+  //  lv_demo_printer();
+  //  lv_demo_stress();             // seems to be OK
   Serial.println("Setup done");
 }
 
@@ -223,33 +192,3 @@ void loop()
   lv_timer_handler(); /* let the GUI do its work */
   delay(5);
 }
-
-// #include "Arduino.h"
-// #include "CST816D.h"
-
-// #define I2C_SDA 4
-// #define I2C_SCL 5
-// #define TP_INT 0
-// #define TP_RST 1
-
-// CST816D touch(I2C_SDA, I2C_SCL, TP_RST, TP_INT);
-
-// void setup()
-// {
-//   Serial.begin(115200);
-//   touch.begin();
-// }
-
-// bool FingerNum;
-// uint8_t gesture;
-// uint16_t touchX, touchY;
-// void loop()
-// {
-//   FingerNum = touch.getTouch(&touchX, &touchY, &gesture);
-//   if (FingerNum)
-//   {
-//     Serial.printf("X:%d,Y:%d,gesture:%x\n", touchX, touchY, gesture);
-//   }
-
-//   delay(100);
-// }
